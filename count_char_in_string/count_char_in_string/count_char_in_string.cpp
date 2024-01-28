5fabe6170709879516468800df161ec5a21edaa0
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2024-01-28 20:30:12 +0300

    count_char_in_string.cpp

---
+++ count_char_in_string/count_char_in_string/count_char_in_string.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	system("chcp 1251");
+	string str = "C++,Java";
+	char ch = '+';
+
+	int count1{};
+	for (int i = 0; (i = str.find(ch, i)) != string::npos; ++i) {
+		count1++;
+	}
+	cout << endl;
+
+	cout << "Символ:\t{"<< ch <<"} встречается "<< count1 <<" раз(а)\n";
+	
+	
+	
+	int count2 = count(str.begin(), str.end(), ch); // второй способ 
+	cout << "Символ:\t{"<< ch <<"} встречается "<< count1 <<" раз(а)\n";
+
+	
+	int count = count_if(str.begin(), str.end(),
+		[&ch](char c) {
+			return c == ch;
+		}); // с помощью лямбда функции 
+
+	cout << "Символ:\t{" << ch << "} встречается " << count1 << " раз(а)\n";
+
+	return 0;
+}

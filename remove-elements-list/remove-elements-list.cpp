810df70c03c88bc2ffffaf91ec1d750caa06f3ab
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2024-01-28 12:44:43 +0300

    Добавьте файлы проекта.

---
+++ remove-elements-list/remove-elements-list.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    list<string> input = { "red", "green", "blue", "gray", "black" };
+
+    list<string>::const_iterator itr = input.cbegin();
+    while (itr != input.cend())
+    {
+        if (itr->length() == 5) {
+            itr = input.erase(itr);
+        }
+        else {
+            ++itr;
+        }
+    }
+
+    copy(input.begin(), input.end(), ostream_iterator<string>(cout, "\n"));
+
+    return 0;
+}
